1cee6998091cc3ef6f4af4dcb84df073a0f3a1d0
Abu Talha Tamim <talha@boishakhikunja.onmicrosoft.com>
2025-04-21 16:44:14 +0600

    1.7: Testimonials Section 20% done

--- Script/script.js
+++ Script/script.js
@@ -6,3 +6,21 @@ $(".counter").counterUp({
   delay: 10,
   time: 1000,
 });
+
+// Slick Slider
+$(".slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".slider-nav",
+});
+$(".slider-nav").slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: ".slider-for",
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  arrows: false,
+});
